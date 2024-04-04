5e1e469f5f39d115ade58b7b47ea9dcb08d660cc
Max Nguyen <trung.nguyenhoangminh1998@gmail.com>
2024-04-04 16:03:45 +0700

    feat: enable hit box and effect using animation event

---
+++ Assets/Scripts/Gameplay/Events/EventListener.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace ArchMageTest.Gameplay.Events
+{
+    public class EventListener : MonoBehaviour
+    {
+        [SerializeField] private VoidEventChannelSO _eventChannel;
+
+        public UnityEvent OnEventRaised;
+
+        private void OnEnable() => _eventChannel.EventRaised += OnResponse;
+
+        private void OnDisable() => _eventChannel.EventRaised -= OnResponse;
+
+        private void OnResponse() => OnEventRaised?.Invoke();
+    }
+}
---
+++ Assets/Scripts/Gameplay/Events/EventRaiser.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace ArchMageTest.Gameplay.Events
+{
+    public class EventRaiser : MonoBehaviour
+    {
+        public void RaiseEvent<T>(GenericEventChannelSO<T> eventChannel, T obj)
+        {
+            eventChannel.RaiseEvent(obj);
+        }
+        
+        public void RaiseEvent(VoidEventChannelSO eventChannel)
+        {
+            eventChannel.RaiseEvent();
+        }
+    }
+}
